721e4fa43fd4b59e61f5c0396d66d72d77a752b6
Александр Евгеньевич Малеев <makedonalexandr060803@gmail.com>
2024-11-04 12:35:41 +0300

    Maybe last commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer: React.FC = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer__title">Faq</div>
+      <div className="footer__title">Contact Us</div>
+      <div className="footer__title">Job</div>
+    </div>
+  );
 };
 
 export default Footer;
